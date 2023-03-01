cfaff710eb2d59d014421f875029e8e08881f6ea
zanezgzheng <zzg.seu@outlook.com>
2023-03-02 00:03:16 +0800

    3.1

---
+++ myconfig/VisDrone-v100/yolov5s/yolov5s.py
@@ -0,0 +1,21 @@
+_base_ = './yolov5_s-v61_syncbn_8xb16-300e_coco.py'
+
+# ======================== wandb & run ==============================
+TAGS = ["origin"]
+GROUP_NAME = "yolov5s"
+ALGO_NAME = "yolov5s"
+DATASET_NAME = "VisDrone"
+
+Wandb_init_kwargs = dict(
+    project=DATASET_NAME,
+    group=GROUP_NAME,
+    name=ALGO_NAME,
+    tags=TAGS
+)
+visualizer = dict(vis_backends = [dict(type='LocalVisBackend'), dict(type='WandbVisBackend', init_kwargs=Wandb_init_kwargs)])
+
+import datetime as dt
+NOW_TIME = dt.datetime.now().strftime('%Y%m%d_%H%M%S')
+work_dir = f"runs/{DATASET_NAME}/{ALGO_NAME}/{NOW_TIME}"
+
+load_from = "https://download.openmmlab.com/mmyolo/v0/yolov5/yolov5_s-v61_syncbn_fast_8xb16-300e_coco/yolov5_s-v61_syncbn_fast_8xb16-300e_coco_20220918_084700-86e02187.pth"
