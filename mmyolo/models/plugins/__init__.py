40baa542a5da596991205cf52b4afb6ad9d91e52
Chunting Duan <duanct@h2.cal.is.tohoku.ac.jp>
2023-03-09 03:34:06 +0900

    TA

--- mmyolo/models/plugins/__init__.py
+++ mmyolo/models/plugins/__init__.py
@@ -1,4 +1,5 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 from .cbam import CBAM
 from .coordattention import CoordAttention
-__all__ = ['CBAM', 'CoordAttention']
+from .TripletAttention import TripletAttention
+__all__ = ['CBAM', 'CoordAttention', 'TripletAttention']
