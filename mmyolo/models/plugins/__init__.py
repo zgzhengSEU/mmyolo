7cf437da58715ca4c67c0c254802c1eb2bc26933
xiegx <1047974352@qq.com>
2023-03-12 03:42:18 +0800

    sa

--- mmyolo/models/plugins/__init__.py
+++ mmyolo/models/plugins/__init__.py
@@ -2,5 +2,5 @@
 from .cbam import CBAM
 from .coordattention import CoordAttention
 from .TripletAttention import TripletAttention
-from .ShuffleAttention.py import ShuffleAttention
+from .ShuffleAttention import ShuffleAttention
 __all__ = ['CBAM', 'CoordAttention', 'TripletAttention', 'ShuffleAttention']
