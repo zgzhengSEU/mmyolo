ff9980dfbbf2aa0f7a95840bfc2dab913d36093e
xiegx <1047974352@qq.com>
2023-03-14 10:06:24 +0800

    1

--- myconfig/VisDrone-seu/yolov7_tiny/yolov7_tiny_tinyp2_AdamW-TA1234-SA1234_ASFFsimCE_VFL-SIOU_noload.py
+++ myconfig/VisDrone-seu/yolov7_tiny/yolov7_tiny_tinyp2_AdamW-TA1234-SA1234_ASFFsimCE_VFL-SIOU_noload.py
@@ -3,7 +3,7 @@ _base_ = './yolov7_l_origin.py'
 # ======================== wandb & run ==============================
 TAGS = ["SEU", "noload", "tinyp2","AdamW", "VFL", "SIOU", "ASFFsim", "TA", "SA"]
 GROUP_NAME = "yolov7_tiny"
-ALGO_NAME = "yolov7_tiny_tinyp2_AdamW-TA1234-SA1234_ASFFsimCE_VFL-SIOU_noload"
+ALGO_NAME = "yolov7_tiny_tinyp2_AdamW_TA1234-SA1234_ASFFsimCE_VFL-SIOU_noload"
 DATASET_NAME = "VisDrone"
 
 Wandb_init_kwargs = dict(
--- myconfig/VisDrone-seu/yolov7_tiny/yolov7_tiny_tinyp2_AdamW_SiLU_TA1234-SA1234_ASFFsimCE_QFL-SIOU.py
+++ myconfig/VisDrone-seu/yolov7_tiny/yolov7_tiny_tinyp2_AdamW_SiLU_TA1234-SA1234_ASFFsimCE_QFL-SIOU.py
@@ -1,9 +1,9 @@
 _base_ = './yolov7_l_origin.py'
 
 # ======================== wandb & run ==============================
-TAGS = ["SEU", "load", "tinyp2","AdamW", "QFL", "SIOU"]
+TAGS = ["SEU", "load", "tinyp2","AdamW", "QFL", "SIOU", "ASFFsim", "TA", "SA", "SiLU"]
 GROUP_NAME = "yolov7_tiny"
-ALGO_NAME = "yolov7_tiny_tinyp2_AdamW_QFL-SIOU"
+ALGO_NAME = "yolov7_tiny_tinyp2_AdamW_SiLU_TA1234-SA1234_ASFFsimCE_QFL-SIOU"
 DATASET_NAME = "VisDrone"
 
 Wandb_init_kwargs = dict(
@@ -70,8 +70,16 @@ img_scale = _base_.img_scale
 pre_transform = _base_.pre_transform
 model = dict(
     backbone=dict(
+        plugins=[
+            dict(
+                cfg=dict(type='TripletAttention'),
+                stages=(True, True, True, True)),
+            dict(
+                cfg=dict(type='ShuffleAttention', groups=16),
+                stages=(True, True, True, True))
+        ],
         arch='Tiny', 
-        act_cfg=dict(type='LeakyReLU', negative_slope=0.1),
+        # act_cfg=dict(type='LeakyReLU', negative_slope=0.1),
         out_indices=(1, 2, 3, 4)),
     # backbone=dict(
     #     arch='Tiny', 
@@ -82,15 +90,24 @@ model = dict(
     #             cfg=dict(type='CBAM'),
     #             stages=(True, True, True, True))
     #     ]),    
-    neck=dict(
-        type='YOLOv7PAFPN4',
-        is_tiny_version=True,
-        in_channels=[64, 128, 256, 512],
-        out_channels=[32, 64, 128, 256],
-        block_cfg=dict(
-            _delete_=True, type='TinyDownSampleBlock', middle_ratio=0.25),
-        act_cfg=dict(type='LeakyReLU', negative_slope=0.1),
-        use_repconv_outs=False),
+    neck=[
+        dict(
+            type='YOLOv7PAFPN4',
+            upsample_feats_cat_first=False,
+            norm_cfg=norm_cfg,
+            is_tiny_version=True,
+            in_channels=[64, 128, 256, 512],
+            out_channels=[32, 64, 128, 256], # 4 层时不会*2
+            block_cfg=dict(
+                type='TinyDownSampleBlock', middle_ratio=0.25),
+            # act_cfg=dict(type='LeakyReLU', negative_slope=0.1),
+            act_cfg=dict(type='SiLU', inplace=True),
+            use_repconv_outs=False),
+        dict(
+            type='ASFFNeck4',
+            widen_factor=0.5,
+            use_carafe=True,
+            use_att='ASFFsim')],
     bbox_head=dict(
         head_module=dict(
             in_channels = [64, 128, 256, 512],
@@ -100,7 +117,7 @@ model = dict(
         loss_cls=dict(
             _delete_=True,
             _scope_='mmdet',
-            type='QualityFocalLoss',
+            type='VarifocalLoss',
             loss_weight=1.0),
         loss_bbox=dict(
             _delete_=True,         
@@ -113,7 +130,7 @@ model = dict(
         loss_obj=dict(            
             _delete_=True,
             _scope_='mmdet',
-            type='QualityFocalLoss',
+            type='VarifocalLoss',
             loss_weight=1.0)))
 
 mosiac4_pipeline = [
